90823c025733fdc9ed66a33d0c2aae050eb70b0c
nicho <nicho.liang@china-rewards.com>
2012-01-30 16:27:29 +0800

    兑换按钮加入事件

--- war/src/main/java-gift/com/chinarewards/gwt/elt/client/awardShopLattice/view/AwardShopLatticeWidget.java
+++ war/src/main/java-gift/com/chinarewards/gwt/elt/client/awardShopLattice/view/AwardShopLatticeWidget.java
@@ -2,8 +2,12 @@ package com.chinarewards.gwt.elt.client.awardShopLattice.view;
 
 import com.chinarewards.gwt.elt.client.awardShopLattice.presenter.AwardShopLatticePresenter.AwardShopLatticeDisplay;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineLabel;
@@ -19,6 +23,8 @@ public class AwardShopLatticeWidget extends Composite implements
 	InlineLabel indate;
 	@UiField
 	Image photo;
+	@UiField
+	Button exchangeBtn;
 	
 	private static AwardShopLatticeWidgetUiBinder uiBinder = GWT
 			.create(AwardShopLatticeWidgetUiBinder.class);
@@ -30,12 +36,20 @@ public class AwardShopLatticeWidget extends Composite implements
 	public AwardShopLatticeWidget() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
-	public AwardShopLatticeWidget(String awardName,String integral,String indate,String photo) {
+	public AwardShopLatticeWidget(final String awardName,final String integral,String indate,String photo) {
 		initWidget(uiBinder.createAndBindUi(this));
 		this.awardName.setText(awardName);
 		this.integral.setText(integral);
 		this.indate.setText(indate);
 		this.photo.setUrl("/imageshow?imageName="+photo);
+		exchangeBtn.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.alert("礼品名称:"+awardName+"-----积分:"+integral);
+				
+			}
+		});
 	}
 
 }
