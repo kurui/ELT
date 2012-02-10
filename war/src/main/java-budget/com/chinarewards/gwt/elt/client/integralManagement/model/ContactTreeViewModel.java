504280d8c3df4288cd565320f97b7c921904f8f2
nicho <nicho.liang@china-rewards.com>
2012-02-10 16:07:57 +0800

    积分管理修改

--- war/src/main/java-budget/com/chinarewards/gwt/elt/client/integralManagement/model/ContactTreeViewModel.java
+++ war/src/main/java-budget/com/chinarewards/gwt/elt/client/integralManagement/model/ContactTreeViewModel.java
@@ -2,22 +2,10 @@ package com.chinarewards.gwt.elt.client.integralManagement.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
-import com.chinarewards.gwt.elt.client.integralManagement.model.ContactDatabase.ContactInfo;
 import com.google.gwt.cell.client.AbstractCell;
-import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.CompositeCell;
-import com.google.gwt.cell.client.FieldUpdater;
-import com.google.gwt.cell.client.HasCell;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.view.client.DefaultSelectionEventManager;
 import com.google.gwt.view.client.ListDataProvider;
-import com.google.gwt.view.client.SelectionModel;
 import com.google.gwt.view.client.TreeViewModel;
 
 /**
@@ -25,14 +13,8 @@ import com.google.gwt.view.client.TreeViewModel;
  */
 public class ContactTreeViewModel implements TreeViewModel {
 
-	/**
-	 * The images used for this example.
-	 */
-	static interface Images extends ClientBundle {
-		ImageResource contact();
 
-		ImageResource contactsGroup();
-	}
+
 
 	/**
 	 * The cell used to render categories.
@@ -71,51 +53,6 @@ public class ContactTreeViewModel implements TreeViewModel {
 	}
 
 	/**
-	 * Tracks the number of contacts in a category that begin with the same
-	 * letter.
-	 */
-	private static class LetterCount implements Comparable<LetterCount> {
-		private final Category category;
-		private final char firstLetter;
-		private int count;
-
-		/**
-		 * Construct a new {@link LetterCount} for one contact.
-		 * 
-		 * @param category
-		 *            the category
-		 * @param firstLetter
-		 *            the first letter of the contacts name
-		 */
-		public LetterCount(Category category, char firstLetter) {
-			this.category = category;
-			this.firstLetter = firstLetter;
-			this.count = 1;
-		}
-
-		public int compareTo(LetterCount o) {
-			return (o == null) ? -1 : (firstLetter - o.firstLetter);
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			return compareTo((LetterCount) o) == 0;
-		}
-
-		@Override
-		public int hashCode() {
-			return firstLetter;
-		}
-
-		/**
-		 * Increment the count.
-		 */
-		public void increment() {
-			count++;
-		}
-	}
-
-	/**
 	 * The Cell used to render a {@link ContactInfo}.
 	 */
 
@@ -148,8 +85,8 @@ public class ContactTreeViewModel implements TreeViewModel {
 			sb.appendHtmlConstant("<tr><td>");
 			sb.appendEscaped("产品部");
 			sb.appendHtmlConstant("</td>");
-			sb.appendHtmlConstant("<td>1600</td>");
-			sb.appendHtmlConstant("<td>5000</td>");
+			sb.appendHtmlConstant("<td>"+value.getBudgetpoints()+"</td>");
+			sb.appendHtmlConstant("<td>"+value.getHasawardedpoints()+"</td>");
 			sb.appendHtmlConstant("<td ><a href='#'>追加</a> <a href='#'>颁奖历史</a></td>");
 			sb.appendHtmlConstant("</tr></table>");
 		}
@@ -169,11 +106,11 @@ public class ContactTreeViewModel implements TreeViewModel {
 
 				sb.appendHtmlConstant("<table style='width:100%'>");
 				sb.appendHtmlConstant("<tr><td>");
-				sb.appendEscaped("部" + value.category.getDisplayName() + " ("
-						+ value.count + ")");
+				sb.appendEscaped( value.getDepartmentsName() + " ("
+						+ value.getCount() + ")");
 				sb.appendHtmlConstant("</td>");
-				sb.appendHtmlConstant("<td>5060</td>");
-				sb.appendHtmlConstant("<td>6000</td>");
+				sb.appendHtmlConstant("<td>"+value.getBudgetpoints()+"</td>");
+				sb.appendHtmlConstant("<td>"+value.getHasawardedpoints()+"</td>");
 				sb.appendHtmlConstant("<td ><a href='#'>追加</a> <a href='#'>颁奖历史</a></td>");
 				sb.appendHtmlConstant("</tr></table>");
 			}
@@ -186,16 +123,14 @@ public class ContactTreeViewModel implements TreeViewModel {
 	// private static Images images;
 
 	private ListDataProvider<Category> categoryDataProvider;
-	private Cell<ContactInfo> contactCell;
-	private final DefaultSelectionEventManager<ContactInfo> selectionManager = DefaultSelectionEventManager
-			.createDefaultManager();
-	private final SelectionModel<ContactInfo> selectionModel;
+//	private Cell<ContactInfo> contactCell;
+//	private final DefaultSelectionEventManager<ContactInfo> selectionManager = DefaultSelectionEventManager
+//			.createDefaultManager();
+//	private final SelectionModel<ContactInfo> selectionModel;
 	final List<Category> categoryList;
 
-	public ContactTreeViewModel(
-			final SelectionModel<ContactInfo> selectionModel,
-			List<Category> categoryList) {
-		this.selectionModel = selectionModel;
+	public ContactTreeViewModel(List<Category> categoryList) {
+//		this.selectionModel = selectionModel;
 		this.categoryList = categoryList;
 
 		// Create a data provider that provides categories.
@@ -216,61 +151,63 @@ public class ContactTreeViewModel implements TreeViewModel {
 		else
 		{
 		    categoryDataProvider = new ListDataProvider<Category>();
-		    List<Category> categoryListx = categoryDataProvider.getList();
-		    for (Category category : ContactDatabase.get().queryCategories()) {
-		      categoryListx.add(category);
-		    }
+//		    List<Category> categoryListx = categoryDataProvider.getList();
+//		    for (Category category : ContactDatabase.get().queryCategories()) {
+//		      categoryListx.add(category);
+//		    }
 		}
-		List<HasCell<ContactInfo, ?>> hasCells = new ArrayList<HasCell<ContactInfo, ?>>();
-		hasCells.add(new HasCell<ContactInfo, ContactInfo>() {
-
-			private ContactCell cell = new ContactCell();
-
-			public Cell<ContactInfo> getCell() {
-				return cell;
-			}
-
-			public FieldUpdater<ContactInfo, ContactInfo> getFieldUpdater() {
-				return null;
-			}
-
-			public ContactInfo getValue(ContactInfo object) {
-				return object;
-			}
-		});
-
-		contactCell = new CompositeCell<ContactInfo>(
-				hasCells) {
-			@Override
-			public void render(Context context,
-					ContactInfo value, SafeHtmlBuilder sb) {
-				sb.appendHtmlConstant("<table style='width:100%'><tbody><tr>");
-				super.render(context, value, sb);
-				sb.appendHtmlConstant("</tr></tbody></table>");
-
-			}
-
-			@Override
-			protected Element getContainerElement(
-					Element parent) {
-				// Return the first TR element in the table.
-				return parent.getFirstChildElement()
-						.getFirstChildElement()
-						.getFirstChildElement();
-			}
-
-			@Override
-			protected <X> void render(Context context,
-					ContactInfo value, SafeHtmlBuilder sb,
-					HasCell<ContactInfo, X> hasCell) {
-				Cell<X> cell = hasCell.getCell();
-				sb.appendHtmlConstant("<td>");
-				cell.render(context,
-						hasCell.getValue(value), sb);
-				sb.appendHtmlConstant("</td>");
-
-			}
-		};
+		
+		//合并其他元素启用以下代码
+//		List<HasCell<ContactInfo, ?>> hasCells = new ArrayList<HasCell<ContactInfo, ?>>();
+//		hasCells.add(new HasCell<ContactInfo, ContactInfo>() {
+//
+//			private ContactCell cell = new ContactCell();
+//
+//			public Cell<ContactInfo> getCell() {
+//				return cell;
+//			}
+//
+//			public FieldUpdater<ContactInfo, ContactInfo> getFieldUpdater() {
+//				return null;
+//			}
+//
+//			public ContactInfo getValue(ContactInfo object) {
+//				return object;
+//			}
+//		});
+//
+//		contactCell = new CompositeCell<ContactInfo>(
+//				hasCells) {
+//			@Override
+//			public void render(Context context,
+//					ContactInfo value, SafeHtmlBuilder sb) {
+//				sb.appendHtmlConstant("<table style='width:100%'><tbody><tr>");
+//				super.render(context, value, sb);
+//				sb.appendHtmlConstant("</tr></tbody></table>");
+//
+//			}
+//
+//			@Override
+//			protected Element getContainerElement(
+//					Element parent) {
+//				// Return the first TR element in the table.
+//				return parent.getFirstChildElement()
+//						.getFirstChildElement()
+//						.getFirstChildElement();
+//			}
+//
+//			@Override
+//			protected <X> void render(Context context,
+//					ContactInfo value, SafeHtmlBuilder sb,
+//					HasCell<ContactInfo, X> hasCell) {
+//				Cell<X> cell = hasCell.getCell();
+//				sb.appendHtmlConstant("<td>");
+//				cell.render(context,
+//						hasCell.getValue(value), sb);
+//				sb.appendHtmlConstant("</td>");
+//
+//			}
+//		};
 		// Construct a composite cell for contacts that includes a checkbox.
 
 	}
@@ -282,35 +219,37 @@ public class ContactTreeViewModel implements TreeViewModel {
 					new CategoryCell());
 		} else if (value instanceof Category) {
 			// Return the first letters of each first name.
-			Category category = (Category) value;
-			List<ContactInfo> contacts = ContactDatabase.get()
-					.queryContactsByCategory(category);
-			Map<Character, LetterCount> counts = new TreeMap<Character, LetterCount>();
-			for (ContactInfo contact : contacts) {
-				Character first = contact.getFirstName().charAt(0);
-				LetterCount count = counts.get(first);
-				if (count == null) {
-					count = new LetterCount(category, first);
-					counts.put(first, count);
-				} else {
-					count.increment();
-				}
+//			Category category = (Category) value;
+	//		List<ContactInfo> contacts = ContactDatabase.get().queryContactsByCategory(category);
+			
+			List<LetterCount> orderedCounts = new ArrayList<LetterCount>();
+			for (int i=1;i<5;i++) {
+				orderedCounts.add(new LetterCount("二级部门"+i,"8000","5000","ID"+i));
 			}
-			List<LetterCount> orderedCounts = new ArrayList<LetterCount>(
-					counts.values());
+			
+			
 			return new DefaultNodeInfo<LetterCount>(
 					new ListDataProvider<LetterCount>(orderedCounts),
 					new LetterCountCell());
 		} else if (value instanceof LetterCount) {
 			// Return the contacts with the specified character and first name.
-			LetterCount count = (LetterCount) value;
-			List<ContactInfo> contacts = ContactDatabase.get()
-					.queryContactsByCategoryAndFirstName(count.category,
-							count.firstLetter + "");
-			ListDataProvider<ContactInfo> dataProvider = new ListDataProvider<ContactInfo>(
-					contacts, ContactInfo.KEY_PROVIDER);
-			return new DefaultNodeInfo<ContactInfo>(dataProvider, contactCell,
-					selectionModel, selectionManager, null);
+//			LetterCount count = (LetterCount) value;
+//			List<ContactInfo> contacts = ContactDatabase.get()
+//					.queryContactsByCategoryAndFirstName(count.category,
+//							count.getDepartmentId()+ "");
+//			
+//			
+//			ListDataProvider<ContactInfo> dataProvider = new ListDataProvider<ContactInfo>(
+//					contacts, ContactInfo.KEY_PROVIDER);
+//			return new DefaultNodeInfo<ContactInfo>(dataProvider, contactCell,
+//					selectionModel, selectionManager, null);
+			List<ContactInfo> orderedCounts = new ArrayList<ContactInfo>();
+			for (int i=1;i<5;i++) {
+				orderedCounts.add(new ContactInfo("3级部门"+i,"9600","1000","ID"+i));
+			}
+			return new DefaultNodeInfo<ContactInfo>(
+					new ListDataProvider<ContactInfo>(orderedCounts),
+					new ContactCell());
 		}
 
 		// Unhandled type.
--- war/src/main/java-budget/com/chinarewards/gwt/elt/client/integralManagement/view/IntegralManagementWidget.java
+++ war/src/main/java-budget/com/chinarewards/gwt/elt/client/integralManagement/view/IntegralManagementWidget.java
@@ -1,12 +1,8 @@
 package com.chinarewards.gwt.elt.client.integralManagement.view;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import com.chinarewards.gwt.elt.client.integralManagement.model.Category;
-import com.chinarewards.gwt.elt.client.integralManagement.model.ContactDatabase;
-import com.chinarewards.gwt.elt.client.integralManagement.model.ContactDatabase.ContactInfo;
 import com.chinarewards.gwt.elt.client.integralManagement.model.ContactTreeViewModel;
 import com.chinarewards.gwt.elt.client.integralManagement.presenter.IntegralManagementPresenter.IntegralManagementDisplay;
 import com.google.gwt.core.client.GWT;
@@ -17,8 +13,6 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.MultiSelectionModel;
-import com.google.gwt.view.client.SelectionChangeEvent;
 
 public class IntegralManagementWidget extends Composite implements
 		IntegralManagementDisplay {
@@ -46,31 +40,31 @@ public class IntegralManagementWidget extends Composite implements
 
 	@Override
 	public void refresh(List<Category> result) {
-		final MultiSelectionModel<ContactInfo> selectionModel = new MultiSelectionModel<ContactInfo>(
-				ContactDatabase.ContactInfo.KEY_PROVIDER);
-		selectionModel
-				.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
-					public void onSelectionChange(SelectionChangeEvent event) {
-						StringBuilder sb = new StringBuilder();
-						boolean first = true;
-						List<ContactInfo> selected = new ArrayList<ContactInfo>(
-								selectionModel.getSelectedSet());
-						Collections.sort(selected);
-						for (ContactInfo value : selected) {
-							if (first) {
-								first = false;
-							} else {
-								sb.append(", ");
-							}
-							sb.append(value.getFullName());
-						}
-						selectedLabel.setText(sb.toString());
-					}
-				});
+//		final MultiSelectionModel<ContactInfo> selectionModel = new MultiSelectionModel<ContactInfo>(
+//				ContactDatabase.ContactInfo.KEY_PROVIDER);
+//		selectionModel
+//				.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+//					public void onSelectionChange(SelectionChangeEvent event) {
+//						StringBuilder sb = new StringBuilder();
+//						boolean first = true;
+//						List<ContactInfo> selected = new ArrayList<ContactInfo>(
+//								selectionModel.getSelectedSet());
+//						Collections.sort(selected);
+//						for (ContactInfo value : selected) {
+//							if (first) {
+//								first = false;
+//							} else {
+//								sb.append(", ");
+//							}
+//							sb.append(value.getFullName());
+//						}
+//						selectedLabel.setText(sb.toString());
+//					}
+//				});
 
 		CellTree.Resources res = GWT.create(CellTree.BasicResources.class);
 
-		CellTree tree = new CellTree(new ContactTreeViewModel(selectionModel,
+		CellTree tree = new CellTree(new ContactTreeViewModel(
 				result), null, res);
 		tree.setAnimationEnabled(true);
 		cellTree.clear();
