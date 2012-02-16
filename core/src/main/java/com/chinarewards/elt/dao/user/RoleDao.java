b5917921f1009a8b7a67187ccdd31c9157001684
nicho <nicho.liang@china-rewards.com>
2012-02-16 11:47:54 +0800

    员工添加..和生成账户..测试类编写

--- core/src/main/java/com/chinarewards/elt/dao/user/RoleDao.java
+++ core/src/main/java/com/chinarewards/elt/dao/user/RoleDao.java
@@ -1,5 +1,7 @@
 package com.chinarewards.elt.dao.user;
 
+import java.util.List;
+
 import com.chinarewards.elt.common.BaseDao;
 import com.chinarewards.elt.domain.user.SysRole;
 import com.chinarewards.elt.model.user.UserRole;
@@ -7,10 +9,14 @@ import com.chinarewards.elt.model.user.UserRole;
 public class RoleDao extends BaseDao<SysRole> {
 
 
+	@SuppressWarnings("unchecked")
 	public SysRole findRoleByRoleName(UserRole roleName) {
-		return (SysRole) getEm()
-				.createQuery("FROM SysRole u WHERE u.name = :roleName")
-				.setParameter("roleName", roleName).getSingleResult();
+		
+		List<SysRole> rolelt=getEm().createQuery("FROM SysRole u WHERE u.name = :roleName").setParameter("roleName", roleName).getResultList();
+		if(rolelt.size()>0)
+		return rolelt.get(0);
+		else
+		return null;
 	}
 	
 
