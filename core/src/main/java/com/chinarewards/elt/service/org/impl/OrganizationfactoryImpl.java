ac3465edfb2dfc50147946e21c88f80484a19ef6
lw <lw@lw-11>
2012-02-20 14:41:38 +0800

    关于奖项的候选小组查找

--- core/src/main/java/com/chinarewards/elt/service/org/impl/OrganizationfactoryImpl.java
+++ core/src/main/java/com/chinarewards/elt/service/org/impl/OrganizationfactoryImpl.java
@@ -4,6 +4,7 @@ import com.chinarewards.elt.domain.org.Corporation;
 import com.chinarewards.elt.domain.org.Department;
 import com.chinarewards.elt.domain.org.Organization;
 import com.chinarewards.elt.domain.org.Staff;
+import com.chinarewards.elt.domain.org.Team;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
@@ -12,15 +13,19 @@ public class OrganizationfactoryImpl implements Organizationfactory {
 	private final OrganizationProcessor staffProcessor;
 	private final OrganizationProcessor corporationProcessor;
 	private final OrganizationProcessor deptmentProcessor;
+	private final OrganizationProcessor teamProcessor;
 
 	@Inject
 	public OrganizationfactoryImpl(
 			@Named("StaffProcessor") OrganizationProcessor staffProcessor,
 			@Named("CorporationProcessor") OrganizationProcessor corporationProcessor,
-			@Named("DeptmentProcessor") OrganizationProcessor deptmentProcessor) {
+			@Named("DeptmentProcessor") OrganizationProcessor deptmentProcessor,
+			@Named("TeamProcessor") OrganizationProcessor teamProcessor
+			) {
 		this.corporationProcessor = corporationProcessor;
 		this.staffProcessor = staffProcessor;
 		this.deptmentProcessor = deptmentProcessor;
+		this.teamProcessor = teamProcessor;
 	}
 
 	public OrganizationProcessor generatorProcessor(Organization organization) {
@@ -31,6 +36,9 @@ public class OrganizationfactoryImpl implements Organizationfactory {
 			res = deptmentProcessor;
 		} else if (organization instanceof Corporation) {
 			res = corporationProcessor;
+		} else if (organization instanceof Team) {
+			res = teamProcessor;
+		
 		} else {
 			throw new IllegalArgumentException("UNKNOW instance organization "
 					+ organization.getClass().getSimpleName());
