eb86873d5dcf63893c5d8d54ecef9abd77c8c0d2
Administrator <Administrator@home-t39v8elt3m>
2012-02-21 11:27:59 +0800

    test git commit

--- war/src/main/java-user/com/chinarewards/gwt/elt/client/department/request/SearchLeaderRequest.java
+++ war/src/main/java-user/com/chinarewards/gwt/elt/client/department/request/SearchLeaderRequest.java
@@ -7,7 +7,7 @@ import net.customware.gwt.dispatch.shared.Action;
 
 /**
  * @author yanrui
- * 
+ * test git commit
  */
 public class SearchLeaderRequest implements Action<SearchLeaderResponse> {
 
