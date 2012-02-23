907516b25b740af9d28bed10bf29940e3fe7116c
nicho <nicho.liang@china-rewards.com>
2012-02-23 11:28:58 +0800

    员工主页面样式修改 and core修改

--- war/src/main/java-information/com/chinarewards/gwt/elt/client/broadcastReplyLattice/view/BroadcastReplyLatticeWidget.java
+++ war/src/main/java-information/com/chinarewards/gwt/elt/client/broadcastReplyLattice/view/BroadcastReplyLatticeWidget.java
@@ -1,15 +1,29 @@
 package com.chinarewards.gwt.elt.client.broadcastReplyLattice.view;
 
 import com.chinarewards.gwt.elt.client.view.constant.ViewConstants;
+import com.chinarewards.gwt.elt.util.StringUtil;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class BroadcastReplyLatticeWidget extends Composite  {
+public class BroadcastReplyLatticeWidget extends Composite {
 
-
+	@UiField
+	InlineLabel deptName;
+	@UiField
+	InlineLabel staffName;
+	@UiField
+	InlineLabel content;
+	@UiField
+	InlineLabel createDate;
+	@UiField
+	InlineLabel createDept;
+	@UiField
+	InlineLabel replyNumber;
 
 	DateTimeFormat dateFormat = DateTimeFormat
 			.getFormat(ViewConstants.date_format);
@@ -20,11 +34,22 @@ public class BroadcastReplyLatticeWidget extends Composite  {
 			UiBinder<Widget, BroadcastReplyLatticeWidget> {
 	}
 
-	public BroadcastReplyLatticeWidget() {
+	public BroadcastReplyLatticeWidget(String deptName, String staffName,
+			String content, String createDate, String createDept,
+			String replyNumber) {
 		initWidget(uiBinder.createAndBindUi(this));
-
+		if (!StringUtil.isEmpty(deptName))
+			this.deptName.setText(deptName);
+		if (!StringUtil.isEmpty(staffName))
+			this.staffName.setText(staffName);
+		if (!StringUtil.isEmpty(content))
+			this.content.setText(content);
+		if (!StringUtil.isEmpty(createDate))
+			this.createDate.setText(createDate);
+		if (!StringUtil.isEmpty(createDept))
+			this.createDept.setText(createDept);
+		if (!StringUtil.isEmpty(replyNumber))
+			this.replyNumber.setText(replyNumber);
 	}
-
-	
 
 }
--- war/src/main/java-staffHeaven/com/chinarewards/gwt/elt/client/staffHeavenIndex/dataprovider/StaffHeavenIndexViewAdapter.java
+++ war/src/main/java-staffHeaven/com/chinarewards/gwt/elt/client/staffHeavenIndex/dataprovider/StaffHeavenIndexViewAdapter.java
@@ -14,6 +14,7 @@ import com.chinarewards.gwt.elt.client.staffHeavenIndex.request.StaffHeavenIndex
 import com.chinarewards.gwt.elt.client.staffHeavenIndex.request.StaffHeavenIndexResponse;
 import com.chinarewards.gwt.elt.client.support.SessionManager;
 import com.chinarewards.gwt.elt.model.PaginationDetailClient;
+import com.chinarewards.gwt.elt.util.DateTool;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Grid;
 
@@ -80,8 +81,8 @@ public class StaffHeavenIndexViewAdapter extends BaseDataProvider<StaffHeavenInd
 				      for (int col = 0; col < numColumns; col++) {
 				    	  if(index<giftList.size())	
 				    	  {
-				    	//	  StaffHeavenIndexClient clint=giftList.get(index);
-				    		  grid.setWidget(row, col,new BroadcastReplyLatticeWidget().asWidget());
+				    		  StaffHeavenIndexClient clint=giftList.get(index);
+				    		  grid.setWidget(row, col,new BroadcastReplyLatticeWidget(null,null,clint.getContent(),DateTool.dateToStringChina2(clint.getBroadcastingTime()),clint.getCreatedByUserName(),clint.getReplyNumber()+"").asWidget());
 				    	  	  index++;
 				    	  }
 				    	  else
