b4af29fd744ef42c74715b4f0cec839199ca733b
nicho <nicho.liang@china-rewards.com>
2012-02-27 15:55:17 +0800

    面包屑..公开返回上一页方法

--- war/src/main/java/com/chinarewards/gwt/elt/client/breadCrumbs/presenter/BreadCrumbsPresenter.java
+++ war/src/main/java/com/chinarewards/gwt/elt/client/breadCrumbs/presenter/BreadCrumbsPresenter.java
@@ -19,6 +19,10 @@ public interface BreadCrumbsPresenter extends	Presenter<BreadCrumbsPresenter.Bre
 	 * 加载其他子页面,调用,传入子菜单的名称
 	 */
 	void loadChildPage(String menuName);
+	/**
+	 * 返回上一页
+	 */
+	void getGoHistory();
 	public static interface BreadCrumbsDisplay extends Display {
 
 		void setTitleText(List<MenuBreadVo> menuBreadVo);
