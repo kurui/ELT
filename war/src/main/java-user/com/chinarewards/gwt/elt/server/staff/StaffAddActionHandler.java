f62f177a832fda37e0a3e100f6cb2ebaa8d961ee
nicho <nicho.liang@china-rewards.com>
2012-03-30 17:32:50 +0800

    修改license

--- war/src/main/java-user/com/chinarewards/gwt/elt/server/staff/StaffAddActionHandler.java
+++ war/src/main/java-user/com/chinarewards/gwt/elt/server/staff/StaffAddActionHandler.java
@@ -69,11 +69,15 @@ public class StaffAddActionHandler extends
 		{
 			throw new ClientException("获取License为空,请联系管理员!");
 		}
+		if(licensebo.getNotafter()==null)
+		{
+			throw new ClientException("获取License过期时间为空,请联系管理员!");
+		}
 		if("FAILED".equals(licensebo))
 		{
 			throw new ClientException(licensebo.getErrorInfo()+",请联系管理员!");
 		}
-		if(licensebo.getNotafter().getTime()<=new Date().getTime())
+		if(licensebo.getNotafter()!=null && licensebo.getNotafter().getTime()<=new Date().getTime())
 		{
 			throw new ClientException("软件License已过期!请重新申请!");
 		}
